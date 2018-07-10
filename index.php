780cc895b30f2aa061f499912fa6f6d14bab285d
Chris Luk <luho@chrisluk.im>
2018-07-10 13:54:42 -0400

    Initial import from crypto site
    Converting base crypto site into full website with subdirectories

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>ONE IN A MILLION</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+				<div class="table100 ver6 m-b-110">
+					<table data-vertable="ver6">
+						<thead>
+							<tr class="row100 head">
+								<th class="column100 column1" data-column="column1">TOTAL: 5 BTC</th>
+								<th class="column100 column2" data-column="column2">Percent</th>
+								<th class="column100 column3" data-column="column3">Value</th>
+								<th class="column100 column4" data-column="column4">Holding</th>
+								<th class="column100 column5" data-column="column5">Last Bought</th>
+								<th class="column100 column6" data-column="column6">for (in BTC)</th>
+								<th class="column100 column7" data-column="column7">Sell Target</th>
+								<th class="column100 column8" data-column="column8">Net Profit</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1"><a href="https://www.binance.com/tradeDetail.html?symbol=TRX_BTC" target="_blank">TRX</a></td>
+								<td class="column100 column2" data-column="column2">50%</td>
+								<td class="column100 column3" data-column="column3">2.4 BTC</td>
+								<td class="column100 column4" data-column="column4">300000</td>
+								<td class="column100 column5" data-column="column5">0.00000793</td>
+								<td class="column100 column6" data-column="column6">for 2.4 BTC</td>
+								<td class="column100 column7" data-column="column7">Next Target: 950</td>
+								<td class="column100 column8" data-column="column8">10-15%</td>
+							</tr>
+
+							<tr class="row100">
+                                <td class="column100 column1" data-column="column1"><a href="https://www.binance.com/tradeDetail.html?symbol=ICX_BTC" target="_blank">ICX</a></td>
+								<td class="column100 column2" data-column="column2">20%</td>
+								<td class="column100 column3" data-column="column3">1.288 BTC</td>
+								<td class="column100 column4" data-column="column4">3500</td>
+								<td class="column100 column5" data-column="column5">0.0003680</td>
+								<td class="column100 column6" data-column="column6">for 1.288 BTC</td>
+								<td class="column100 column7" data-column="column7">???</td>
+								<td class="column100 column8" data-column="column8">14% profit 1.47 BTC</td>
+							</tr>
+							<tr class="row100">
+                                <td class="column100 column1" data-column="column1"><a href="https://www.binance.com/tradeDetail.html?symbol=IOTA_BTC" target="_blank">IOTA</a></td>
+								<td class="column100 column2" data-column="column2">20%</td>
+								<td class="column100 column3" data-column="column3">0.71415 BTC</td>
+								<td class="column100 column4" data-column="column4">3000</td>
+								<td class="column100 column5" data-column="column5">0.00023805</td>
+								<td class="column100 column6" data-column="column6">for 0.71415 BTC</td>
+								<td class="column100 column7" data-column="column7">???</td>
+								<td class="column100 column8" data-column="column8">???</td>
+							</tr>
+
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1"><a href="https://www.binance.com/tradeDetail.html?symbol=OMG_BTC" target="_blank">OMG</a></td>
+								<td class="column100 column2" data-column="column2">5%</td>
+								<td class="column100 column3" data-column="column3">0.4158 BTC</td>
+								<td class="column100 column4" data-column="column4">270</td>
+								<td class="column100 column5" data-column="column5">0.00154</td>
+								<td class="column100 column6" data-column="column6">for 0.4158 BTC</td>
+								<td class="column100 column7" data-column="column7">???</td>
+								<td class="column100 column8" data-column="column8">???</td>
+							</tr>
+
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1"><a href="https://www.binance.com/tradeDetail.html?symbol=ADA_BTC" target="_blank">ADA</a></td>
+								<td class="column100 column2" data-column="column2">5%</td>
+								<td class="column100 column3" data-column="column3">0.14855 BTC</td>
+								<td class="column100 column4" data-column="column4">5000</td>
+								<td class="column100 column5" data-column="column5">0.00002971</td>
+								<td class="column100 column6" data-column="column6">for 0.14855 BTC</td>
+								<td class="column100 column7" data-column="column7">???</td>
+								<td class="column100 column8" data-column="column8">???</td>
+							</tr>
+<!--
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1"><a href="https://www.binance.com/tradeDetail.html?symbol=BTS_BTC">ICX</a></td>
+								<td class="column100 column2" data-column="column2">10%</td>
+								<td class="column100 column3" data-column="column3">0.7 BTC</td>
+								<td class="column100 column4" data-column="column4"></td>
+								<td class="column100 column5" data-column="column5"></td>
+								<td class="column100 column6" data-column="column6"></td>
+								<td class="column100 column7" data-column="column7"></td>
+								<td class="column100 column8" data-column="column8"></td>
+							</tr>
+
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1"><a href="https://www.binance.com/tradeDetail.html?symbol=BNB_BTC" target="_blank">IOTA</a></td>
+								<td class="column100 column2" data-column="column2">10%</td>
+								<td class="column100 column3" data-column="column3">0.7 BTC</td>
+								<td class="column100 column4" data-column="column4"></td>
+								<td class="column100 column5" data-column="column5"></td>
+								<td class="column100 column6" data-column="column6"></td>
+								<td class="column100 column7" data-column="column7"></td>
+								<td class="column100 column8" data-column="column8"></td>
+							</tr>
+
+							<tr class="row100">
+								<td class="column100 column1" data-column="column1"><a href="https://www.binance.com/tradeDetail.html?symbol=NEO_BTC" target="_blank">NEO</a></td>
+								<td class="column100 column2" data-column="column2">20%</td>
+								<td class="column100 column3" data-column="column3"></td>
+								<td class="column100 column4" data-column="column4"></td>
+								<td class="column100 column5" data-column="column5"></td>
+								<td class="column100 column6" data-column="column6"></td>
+								<td class="column100 column7" data-column="column7"></td>
+								<td class="column100 column8" data-column="column8"></td>
+							</tr>-->
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	
+
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>

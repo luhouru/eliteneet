06af0b99e1ab914c9f026594e32fc77d17f1dc8c
Chris Luk <luho@chrisluk.im>
2018-07-10 14:18:29 -0400

    just changed homepage a bit
    added link to crypto folder

--- index.php
+++ index.php
@@ -8,21 +8,23 @@
 <body>
     
 <?php
-echo "I'm not your average NEET schmuck. I chose the NEET life. I am an elite NEET. Welcome to my NEET portfolio."
+    
+echo "I'm not your average NEET schmuck. I chose the NEET life. I am an elite NEET. Welcome to my NEET portfolio. <br><br>";
 
     
-echo "Planned Sections:"; <br><br>
+echo "Planned Sections: <br><br>";
 
     
+$url="/crypto";
+$site_title="cryptocurrency";
+
     
-    
-    
+echo "1. <a href=$url>$site_title</a><br>
+
+2. anime favorites by genre<br>
+3. list of video games I've played<br>
+4. goals for next year as elite neet number 1.<br>";
+
 ?>
-    
-1. crypto
-2. anime favorites by genre
-3. list of video games I've played
-4. goals for next year as elite neet number 1.
-
 </body>
 </html>
